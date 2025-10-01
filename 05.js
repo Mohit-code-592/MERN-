63b3b62018fd06821bbef459d13e22e320587238
Mohit-code-592 <mr.mohitkumar592@gmail.com>
2025-10-01 16:06:17 +0530

    Create 05.js

---
+++ 05.js
@@ -0,0 +1,22 @@
+let gender = "Male";
+let age = 33;
+
+if(gender == "Male") {
+    console.log("Go to Buiding A");
+    if(age <= 33) {
+        console.log("Go to block A");
+    }
+    else {
+        console.log("Go to block B");
+    }
+}
+else {
+    console.log("Go to building B");
+    if(age <= 33) {
+        console.log("Go to block A");
+    }
+    else {
+        console.log("Go to block B");
+    }
+}
+
